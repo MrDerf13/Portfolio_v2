185306b20706f9173cdeaa13dc7fa6b1ae50ba35
MrDerf13 <gia_fred@hotmail.com>
2024-03-11 10:48:13 +1100

    changes to image file paths

--- src/Components/ProjectCard/ProjectCard.jsx
+++ src/Components/ProjectCard/ProjectCard.jsx
@@ -8,7 +8,11 @@ const ProjectCard = ({ obj }) => {
       <p>{obj.description}</p>
       <div>
         <a href={obj.link}>
-          <img src="github.png" alt="Github" className={styles.gitImg} />
+          <img
+            src="./src/assets/github.png"
+            alt="Github"
+            className={styles.gitImg}
+          />
         </a>
       </div>
     </article>
